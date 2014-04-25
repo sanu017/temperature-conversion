4f1e6a57693c821100285b5842010ffd0a36ed84
sanu017 <shukla.anubhav@hotmail.com>
2014-04-25 14:53:20 +0530

    Added sample  functional testcase

--- src/test/java/com/anubhav/mule/FunctionalTest.java
+++ src/test/java/com/anubhav/mule/FunctionalTest.java
@@ -12,7 +12,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-public class TemperatureConversion extends FunctionalTestCase{
+public class FunctionalTest extends FunctionalTestCase{
 
 	@Override
 	protected String getConfigResources() {
